a8ba78c918288f798cf75deb2f20c118e55f5cfd
hialdev <al.developer12@gmail.com>
2022-11-15 17:48:27 +0700

    first dev

---
+++ src/js/carousel.js
@@ -0,0 +1,41 @@
+$('.project-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    autoplay:true,
+    dots : true,
+    autoWidth: true,
+    nav : false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('.news-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    autoplay:true,
+    dots : true,
+    autoWidth: true,
+    nav : false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
