8489b1859239e54e0147799c45b3bf9bfedc3bd0
hialdev <al.developer12@gmail.com>
2022-11-20 18:57:11 +0700

    Full Frontend

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    
+    $(".menu-open").click(function(){
+        $(".menu").addClass('active');
+    })
+    $(".menu-close").click(function(){
+        $(".menu").removeClass('active');
+    })
+
+})
